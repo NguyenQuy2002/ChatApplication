3553a54bc4e0bacad93319e105ba5cabafce7451
NguyenQuy2002 <quy.nguyen2002@hcmut.edu.vn>
2022-12-08 15:17:38 +0700

    change chat client

--- client/client.py
+++ client/client.py
@@ -5,146 +5,267 @@ from random import randint
 import os
 
 
-def register():
-    global register_screen
-    register_screen = Toplevel(main_screen)
-    register_screen.title("Register")
-    register_screen.geometry("400x300")
+class GUI():
+    def __init__(self):
+        self.main_screen = Tk()
+        self.main_screen.geometry("300x250")
+        self.main_screen.title("Account Login")
 
-    global username
-    global password
-    global username_entry
-    global password_entry
-    username = StringVar()
-    password = StringVar()
+        Label(text="Select Your Choice", bg="orange", width="300",
+              height="2", font=("Calibri", 13)).pack()
+        Label(text="").pack()
+        Button(text="Login", height="3", width="30", command=self.login).pack()
+        Label(text="").pack()
+        Button(text="Register", height="3", width="30",
+               command=self.register).pack()
 
-    Label(register_screen, text="Please enter details below",
-          bg="orange", font='Calibri 16 bold', width="400", height="2").pack()
-    Label(register_screen, text="").pack()
-    username_label = Label(
-        register_screen, text="New Username:", font='Calibri 13 bold')
-    username_label.pack()
-    username_entry = Entry(
-        register_screen, textvariable=username, font='Calibri 13 bold')
-    username_entry.pack()
-    username_label = Label(
-        register_screen, text="New Password:", font='Calibri 13 bold')
-    username_label.pack()
-    password_entry = Entry(
-        register_screen, textvariable=password, show='*', font='Calibri 13 bold')
-    password_entry.pack()
-    Label(register_screen, text="").pack()
-    Button(register_screen, text="Register", width=30,
-           height=1, bg="orange", command=register_user).pack()
+        self.main_screen.mainloop()
 
+    def register(self):
+        self.register_screen = Toplevel(self.main_screen)
+        self.register_screen.title("Account Register")
+        self.register_screen.geometry("400x300")
 
-def register_user():
+        self.username = StringVar()
+        self.password = StringVar()
 
-    username_info = username.get()
-    password_info = password.get()
+        Label(self.register_screen, text="Please enter details below",
+              bg="orange", font='Calibri 16 bold', width="400", height="2").pack()
+        Label(self.register_screen, text="").pack()
 
-# Open file in write mode
-    file = open(username_info, "w")
+        self.username_label = Label(
+            self.register_screen, text="New Username:", font='Calibri 13 bold')
+        self.username_label.pack()
 
-# write username and password information into file
-    file.write(username_info + "\n")
-    file.write(password_info)
-    file.close()
+        self.username_entry = Entry(
+            self.register_screen, textvariable=self.username, font='Calibri 13 bold')
+        self.username_entry.pack()
 
-    username_entry.delete(0, END)
-    password_entry.delete(0, END)
+        self.username_label = Label(
+            self.register_screen, text="New Password:", font='Calibri 13 bold')
+        self.username_label.pack()
 
-    Label(register_screen, text="Registration Success",
-          fg="green", font=("calibri", 11)).pack()
+        self.password_entry = Entry(
+            self.register_screen, textvariable=self.password, show='*', font='Calibri 13 bold')
+        self.password_entry.pack()
 
+        Label(self.register_screen, text="").pack()
+        Button(self.register_screen, text="Register", width=30,
+               height=1, bg="orange", command=self.register_user).pack()
 
-def login():
-    global login_screen
-    login_screen = Toplevel(main_screen)
-    login_screen.title("Login")
-    login_screen.geometry("400x300")
-    Label(login_screen, text="Please enter details below to login", bg='orange',
-          font='Calibri 16 bold', width="400", height="2").pack()
+    def login(self):
+        self.login_screen = Toplevel(self.main_screen)
+        self.login_screen.title("Login")
+        self.login_screen.geometry("400x300")
+        Label(self.login_screen, text="Please enter details below to login", bg='orange',
+              font='Calibri 16 bold', width="400", height="2").pack()
 
-    global username_verify
-    global password_verify
+        self.username_verify = StringVar()
+        self.password_verify = StringVar()
 
-    username_verify = StringVar()
-    password_verify = StringVar()
+        Label(self.login_screen, text="").pack()
+        Label(self.login_screen, text="Username:",
+              font='Calibri 13 bold').pack()
+        self.username_login_entry = Entry(
+            self.login_screen, textvariable=self.username_verify, font='Calibri 13 bold')
+        self.username_login_entry.pack()
+        Label(self.login_screen, text="Password:",
+              font='Calibri 13 bold').pack()
+        self.password_login_entry = Entry(
+            self.login_screen, textvariable=self.password_verify, show='*', font='Calibri 13 bold')
+        self.password_login_entry.pack()
+        Label(self.login_screen, text="").pack()
+        # Button(login_screen, text="Login", width=30, height=1, command=login_verify).pack()
+        Button(self.login_screen, text="Login", width=30,
+               height=1, command=self.chat_layout).pack()
 
-    global username_login_entry
-    global password_login_entry
+    def register_user(self):
 
-    Label(login_screen, text="").pack()
-    Label(login_screen, text="Username:", font='Calibri 13 bold').pack()
-    username_login_entry = Entry(
-        login_screen, textvariable=username_verify, font='Calibri 13 bold')
-    username_login_entry.pack()
-    Label(login_screen, text="Password:", font='Calibri 13 bold').pack()
-    password_login_entry = Entry(
-        login_screen, textvariable=password_verify, show='*', font='Calibri 13 bold')
-    password_login_entry.pack()
-    Label(login_screen, text="").pack()
-    Button(login_screen, text="Login", width=30,
-           height=1, command=login_verify).pack()
+        self.username_info = self.username.get()
+        self.password_info = self.password.get()
 
+    # Open file in write mode
+        self.file = open(self.username_info, "w")
 
-def login_verify():
+    # write username and password information into file
+        self.file.write(self.username_info + "\n")
+        self.file.write(self.password_info)
+        self.file.close()
 
-    username1 = username_verify.get()
-    password1 = password_verify.get()
-    
-    username_login_entry.delete(0, END)
-    password_login_entry.delete(0, END)
+        self.username_entry.delete(0, END)
+        self.password_entry.delete(0, END)
 
-    list_of_files = os.listdir()
+        Label(self.register_screen, text="Registration Success",
+              fg="green", font=("Calibri", 11)).pack()
 
-    if username1 in list_of_files:
-        file1 = open(username1, "r")
+    def login_verify(self):
 
-        verify = file1.read().splitlines()
-        if password1 in verify:
-            login_success()
+        self.username1 = self.username_verify.get()
+        self.password1 = self.password_verify.get()
+
+        self.username_login_entry.delete(0, END)
+        self.password_login_entry.delete(0, END)
+
+        list_of_files = os.listdir()
+
+        if self.username1 in list_of_files:
+            file1 = open(self.username1, "r")
+
+            verify = file1.read().splitlines()
+            if self.password1 in verify:
+                login_success()
+            else:
+                self.password_not_recognised()
         else:
-            password_not_recognised()
-    else:
-        print('User not found!')
+
+            print('User not found!')
+
+    def login_success(self):
+
+        self.login_success_screen = Toplevel(self.login_screen)
+        self.login_success_screen.title("Success")
+        self.login_success_screen.geometry("150x100")
+        Label(self.login_success_screen, text="Login Success").pack()
+
+        Button(self.login_success_screen, text="OK",
+               command=self.login_success_screen.destroy()).pack()
+
+    def password_not_recognised(self):
+        self.password_not_recog_screen = Toplevel(self.login_screen)
+        self.password_not_recog_screen.title("Success")
+        self.password_not_recog_screen.geometry("150x100")
+        Label(self.password_not_recog_screen, text="Invalid Password ").pack()
+        Button(self.password_not_recog_screen, text="OK",
+               command=self.password_not_recog_screen.destroy()).pack()
+
+    def chat_layout(self):
+        self.name = self.username_login_entry
+        # to show chat window
+        self.Window = Tk()
+        self.Window.withdraw()
+        self.Window.deiconify()
+        self.Window.title("Chat room")
+        self.Window.resizable(width=False,
+                              height=False)
+        self.Window.configure(width=470,
+                              height=550,
+                              bg="#17202A")
+        self.labelHead = Label(self.Window,
+                               bg="#17202A",
+                               fg="#EAECEE",
+                               text=self.name,
+                               font="Helvetica 13 bold",
+                               pady=5)
+
+        self.labelHead.place(relwidth=1)
+        self.line = Label(self.Window,
+                          width=450,
+                          bg="#ABB2B9")
+
+        self.line.place(relwidth=1,
+                        rely=0.07,
+                        relheight=0.012)
+
+        self.textCons = Text(self.Window,
+                             width=20,
+                             height=2,
+                             bg="#17202A",
+                             fg="#EAECEE",
+                             font="Helvetica 14",
+                             padx=5,
+                             pady=5)
+
+        self.textCons.place(relheight=0.745,
+                            relwidth=1,
+                            rely=0.08)
+
+        self.labelBottom = Label(self.Window,
+                                 bg="#ABB2B9",
+                                 height=80)
+
+        self.labelBottom.place(relwidth=1,
+                               rely=0.825)
+
+        self.entryMsg = Entry(self.labelBottom,
+                              bg="#2C3E50",
+                              fg="#EAECEE",
+                              font="Helvetica 13")
+
+        # place the given widget
+        # into the gui window
+        self.entryMsg.place(relwidth=0.74,
+                            relheight=0.06,
+                            rely=0.008,
+                            relx=0.011)
+
+        self.entryMsg.focus()
+
+        # create a Send Button
+        self.buttonMsg = Button(self.labelBottom,
+                                text="Send",
+                                font="Helvetica 10 bold",
+                                width=20,
+                                bg="#ABB2B9",
+                                command=lambda: self.sendButton(self.entryMsg.get()))
+
+        self.buttonMsg.place(relx=0.77,
+                             rely=0.008,
+                             relheight=0.06,
+                             relwidth=0.22)
+
+        self.textCons.config(cursor="arrow")
+
+        # create a scroll bar
+        self.scrollbar = Scrollbar(self.textCons)
+
+        # place the scroll bar
+        # into the gui window
+        self.scrollbar.place(relheight=1,
+                             relx=0.974)
+
+        self.scrollbar.config(command=self.textCons.yview)
+
+        self.textCons.config(state=DISABLED)
+
+    def sendButton(self, msg):
+        self.textCons.config(state=DISABLED)
+        self.msg = msg
+        self.entryMsg.delete(0, END)
+        snd = threading.Thread(target=self.sendMessage)
+        snd.start()
+
+    # function to receive messages
+
+    # def receive(self):
+    #     while True:
+    #         try:
+    #             message = client.recv(1024).decode(FORMAT)
+
+    #             # if the messages from the server is NAME send the client's name
+    #             if message == 'NAME':
+    #                 client.send(name.encode(FORMAT))
+    #             else:
+    #                 # insert messages to text box
+    #                 textCons.config(state=NORMAL)
+    #                 textCons.insert(END,
+    #                                 message+"\n\n")
+
+    #                 textCons.config(state=DISABLED)
+    #                 textCons.see(END)
+    #         except:
+    #             # an error will be printed on the command line or console if there's an error
+    #             print("An error occurred!")
+    #             client.close()
+    #             break
+
+    # # function to send messages
+
+    # def sendMessage(self):
+    #     self.textCons.config(state=DISABLED)
+    #     while True:
+    #         message = (f"{name}: {msg}")
+    #         client.send(message.encode(FORMAT))
+    #         break
 
 
-def login_success():
-    global login_success_screen
-    login_success_screen = Toplevel(login_screen)
-    login_success_screen.title("Success")
-    login_success_screen.geometry("150x100")
-    Label(login_success_screen, text="Login Success").pack()
-
-    Button(login_success_screen, text="OK",
-           command=login_success_screen.destroy()).pack()
-    
-
-def password_not_recognised():
-    global password_not_recog_screen
-    password_not_recog_screen = Toplevel(login_screen)
-    password_not_recog_screen.title("Success")
-    password_not_recog_screen.geometry("150x100")
-    Label(password_not_recog_screen, text="Invalid Password ").pack()
-    Button(password_not_recog_screen, text="OK",
-           command=password_not_recog_screen.destroy()).pack()
-
-
-def main_account_screen():
-    global main_screen
-    main_screen = Tk()
-    main_screen.geometry("300x250")
-    main_screen.title("Account Login")
-    Label(text="Select Your Choice", bg="orange", width="300",
-          height="2", font=("Calibri", 13)).pack()
-    Label(text="").pack()
-    Button(text="Login", height="3", width="30", command=login).pack()
-    Label(text="").pack()
-    Button(text="Register", height="3", width="30", command=register).pack()
-
-    main_screen.mainloop()
-
-
-main_account_screen()
+g = GUI()
